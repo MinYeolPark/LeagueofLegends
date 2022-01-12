1b1dc20130dbe09b8858889e32ba744f47cc1453
박민열 <67354549+MinYeolPark@users.noreply.github.com>
2022-01-12 17:17:35 +0900

    01/12
    Damage Popup Update

--- Assets/Scripts/10.UI/DamagePopup.cs
+++ Assets/Scripts/10.UI/DamagePopup.cs
@@ -1,11 +1,63 @@
 using UnityEngine;
+using TMPro;
 
 public class DamagePopup : MonoBehaviour
 {
+    private TextMeshProUGUI textMesh;
+    private float disappearTimer;
+    private Color textColor;
     [SerializeField] private Transform pfDamagePopup;
 
-    private void Start()
+    public static DamagePopup Create(Vector3 position, int damageAmount, bool isCriticalHit)
     {
-        Instantiate(pfDamagePopup, Vector3.zero, Quaternion.identity);
+        Transform damagePopupTransform = Instantiate(GameAssets.Instance.pfDamagePopup, position, Quaternion.identity);
+
+        DamagePopup damagePopup = damagePopupTransform.GetComponent<DamagePopup>();
+        damagePopup.Setup(damageAmount, isCriticalHit);
+
+        return damagePopup;
+    }
+
+    private void Awake()
+    {
+        textMesh = transform.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void Setup(int damageAmount, bool isCriticalHit)
+    {
+        textMesh.SetText(damageAmount.ToString());
+        if(!isCriticalHit)
+        {
+            //normalhit
+            textMesh.fontSize = 36f;
+            //textColor=
+        }
+        else
+        {
+            textMesh.fontSize = 42f;
+        }
+        textMesh.color = textColor;
+        textMesh.geometrySortingOrder++;
+        disappearTimer = 1f;
+    }
+    private void Update()
+    {
+        float moveYSpeed = 20f;
+        transform.position += new Vector3(0,moveYSpeed*Time.deltaTime);
+
+        disappearTimer -= Time.deltaTime;
+
+        if (disappearTimer < 0)
+        {
+            //Start disappearing
+            float disappearSpeed = 3f;
+            textColor.a -= disappearSpeed * Time.deltaTime;
+            textMesh.color = textColor;
+
+            if (textColor.a < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
